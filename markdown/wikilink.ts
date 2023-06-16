afc7184ca875f8fcb44c538a93d768ed75239406
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2023-06-16 15:25:14 +0700

    feat: add wikilink parser

---
+++ markdown/wikilink.ts
@@ -0,0 +1,121 @@
+import { Code, Effects, Extension, State } from "micromark-util-types";
+
+declare module "micromark-util-types" {
+  export interface TokenTypeMap {
+    wikiLink: "wikiLink";
+    wikiLinkMarker: "wikiLinkMarker";
+    wikiLinkData: "wikiLinkData";
+    wikiLinkTitlePipe: "wikiLinkTitlePipe";
+  }
+}
+
+const startMarker = "[[";
+const endMarker = "]]";
+const aliasPipe = "|";
+
+const codes = {
+  horizontalTab: -2,
+  virtualSpace: -1,
+  nul: 0,
+  eof: null,
+  space: 32,
+};
+
+function markdownLineEndingOrSpace(code: Code) {
+  return code == codes.eof || code < codes.nul || code === codes.space;
+}
+
+function markdownLineEnding(code: Code) {
+  return code && code < codes.horizontalTab;
+}
+
+function wikiLinkTokenize(effects: Effects, ok: State, nok: State): State {
+  let startMarkerCursor = 0;
+  let endMarkerCursor = 0;
+  let dataLength = 0;
+
+  return start;
+
+  function start(code: Code): State {
+    effects.enter("wikiLink");
+    effects.enter("wikiLinkMarker");
+
+    return consumerStartMarker(code);
+  }
+
+  function consumerStartMarker(code: Code): State {
+    if (code !== startMarker.charCodeAt(startMarkerCursor)) {
+      return nok(code) as State;
+    }
+
+    effects.consume(code);
+    startMarkerCursor++;
+
+    if (startMarkerCursor === startMarker.length) {
+      effects.exit("wikiLinkMarker");
+      effects.enter("wikiLinkData");
+      return consumeData;
+    }
+    return consumerStartMarker;
+  }
+
+  function consumeData(code: Code): State {
+    if (code === endMarker.charCodeAt(0)) {
+      if (dataLength == 0) return nok(code) as State;
+      effects.exit("wikiLinkData");
+      effects.enter("wikiLinkMarker");
+      return consumeEndMarker(code);
+    }
+
+    if (code === aliasPipe.charCodeAt(0)) {
+      if (dataLength == 0) return nok(code) as State;
+      effects.exit("wikiLinkData");
+      effects.enter("wikiLinkTitlePipe");
+      return consumeAliasPipe(code);
+    }
+
+    if (markdownLineEnding(code) || code === codes.eof) {
+      return nok(code) as State;
+    }
+
+    if (!markdownLineEndingOrSpace(code)) {
+      dataLength++;
+    }
+
+    effects.consume(code);
+    return consumeData;
+  }
+
+  function consumeAliasPipe(code: Code): State {
+    dataLength = 0;
+    effects.consume(code);
+    effects.exit("wikiLinkTitlePipe");
+    effects.enter("wikiLinkData");
+    return consumeData;
+  }
+
+  function consumeEndMarker(code: Code): State {
+    if (code !== endMarker.charCodeAt(endMarkerCursor)) {
+      return nok(code) as State;
+    }
+
+    effects.consume(code);
+    endMarkerCursor++;
+
+    if (endMarkerCursor === endMarker.length) {
+      effects.exit("wikiLinkMarker");
+      effects.exit("wikiLink");
+      return ok;
+    }
+    return consumeEndMarker;
+  }
+}
+
+export const wikiLinkExtension: Extension = {
+  text: {
+    91: {
+      name: "wikiLink",
+      tokenize: wikiLinkTokenize,
+    },
+  },
+};
---
+++ markdown/wikilink_test.ts
@@ -0,0 +1,186 @@
+import { Event } from "micromark-util-types";
+import { parse } from "micromark/lib/parse.js";
+import { preprocess } from "micromark/lib/preprocess.js";
+import { postprocess } from "micromark/lib/postprocess.js";
+import { assertEquals } from "std/testing/asserts.ts";
+import { wikiLinkExtension } from "./wikilink.ts";
+
+type SimplifiedEvent = [Event[0], Event[1]["type"], string];
+
+function micromarkEvents(md: string): SimplifiedEvent[] {
+  return postprocess(
+    parse({
+      extensions: [wikiLinkExtension],
+    })
+      .document()
+      .write(preprocess()(md, "utf8", true))
+  ).map((event) => {
+    return [event[0], event[1].type, event[2].sliceSerialize(event[1])];
+  });
+}
+
+Deno.test("parse wikilink without alias", () => {
+  const content = "[[Wiki Link]]";
+  assertEquals(micromarkEvents(content), [
+    ["enter", "content", content],
+    ["enter", "paragraph", content],
+    ["enter", "wikiLink", content],
+    ["enter", "wikiLinkMarker", "[["],
+    ["exit", "wikiLinkMarker", "[["],
+    ["enter", "wikiLinkData", "Wiki Link"],
+    ["exit", "wikiLinkData", "Wiki Link"],
+    ["enter", "wikiLinkMarker", "]]"],
+    ["exit", "wikiLinkMarker", "]]"],
+    ["exit", "wikiLink", content],
+    ["exit", "paragraph", content],
+    ["exit", "content", content],
+  ] as SimplifiedEvent[]);
+});
+
+Deno.test("parse wikilink with title", () => {
+  const content = "[[Link Title|Wiki Link]]";
+  assertEquals(micromarkEvents(content), [
+    ["enter", "content", content],
+    ["enter", "paragraph", content],
+    ["enter", "wikiLink", content],
+    ["enter", "wikiLinkMarker", "[["],
+    ["exit", "wikiLinkMarker", "[["],
+    ["enter", "wikiLinkData", "Link Title"],
+    ["exit", "wikiLinkData", "Link Title"],
+    ["enter", "wikiLinkTitlePipe", "|"],
+    ["exit", "wikiLinkTitlePipe", "|"],
+    ["enter", "wikiLinkData", "Wiki Link"],
+    ["exit", "wikiLinkData", "Wiki Link"],
+    ["enter", "wikiLinkMarker", "]]"],
+    ["exit", "wikiLinkMarker", "]]"],
+    ["exit", "wikiLink", content],
+    ["exit", "paragraph", content],
+    ["exit", "content", content],
+  ] as SimplifiedEvent[]);
+});
+
+Deno.test("handle open wikilink", () => {
+  const content = "t[[\nt";
+  assertEquals(micromarkEvents(content), [
+    ["enter", "content", content],
+    ["enter", "paragraph", content],
+    ["enter", "data", "t"],
+    ["exit", "data", "t"],
+    ["enter", "data", "["],
+    ["exit", "data", "["],
+    ["enter", "data", "["],
+    ["exit", "data", "["],
+    ["enter", "lineEnding", "\n"],
+    ["exit", "lineEnding", "\n"],
+    ["enter", "data", "t"],
+    ["exit", "data", "t"],
+    ["exit", "paragraph", content],
+    ["exit", "content", content],
+  ] as SimplifiedEvent[]);
+});
+
+Deno.test("handles open wiki links at end of file", () => {
+  const content = "t [[";
+  assertEquals(micromarkEvents(content), [
+    ["enter", "content", content],
+    ["enter", "paragraph", content],
+    ["enter", "data", "t "],
+    ["exit", "data", "t "],
+    ["enter", "data", "["],
+    ["exit", "data", "["],
+    ["enter", "data", "["],
+    ["exit", "data", "["],
+    ["exit", "paragraph", content],
+    ["exit", "content", content],
+  ] as SimplifiedEvent[]);
+});
+Deno.test("handles open wiki links with partial data", () => {
+  const content = "t [[tt\nt";
+  assertEquals(micromarkEvents(content), [
+    ["enter", "content", content],
+    ["enter", "paragraph", content],
+    ["enter", "data", "t "],
+    ["exit", "data", "t "],
+    ["enter", "data", "["],
+    ["exit", "data", "["],
+    ["enter", "data", "["],
+    ["exit", "data", "["],
+    ["enter", "data", "tt"],
+    ["exit", "data", "tt"],
+    ["enter", "lineEnding", "\n"],
+    ["exit", "lineEnding", "\n"],
+    ["enter", "data", "t"],
+    ["exit", "data", "t"],
+    ["exit", "paragraph", content],
+    ["exit", "content", content],
+  ] as SimplifiedEvent[]);
+});
+
+Deno.test("handles open wiki links with partial alias", () => {
+  const content = "[[t|\nt";
+  assertEquals(micromarkEvents(content), [
+    ["enter", "content", content],
+    ["enter", "paragraph", content],
+    ["enter", "data", "["],
+    ["exit", "data", "["],
+    ["enter", "data", "["],
+    ["exit", "data", "["],
+    ["enter", "data", "t|"],
+    ["exit", "data", "t|"],
+    ["enter", "lineEnding", "\n"],
+    ["exit", "lineEnding", "\n"],
+    ["enter", "data", "t"],
+    ["exit", "data", "t"],
+    ["exit", "paragraph", content],
+    ["exit", "content", content],
+  ] as SimplifiedEvent[]);
+});
+
+Deno.test("handle invalid wikilink with blank data", () => {
+  const content = "[[]]";
+  assertEquals(micromarkEvents(content), [
+    ["enter", "content", content],
+    ["enter", "paragraph", content],
+    ["enter", "data", "["],
+    ["exit", "data", "["],
+    ["enter", "data", "["],
+    ["exit", "data", "["],
+    ["enter", "data", "]]"],
+    ["exit", "data", "]]"],
+    ["exit", "paragraph", content],
+    ["exit", "content", content],
+  ] as SimplifiedEvent[]);
+});
+
+Deno.test("handle invalid wikilink with blank before pipe", () => {
+  const content = "[[|Test]]";
+  assertEquals(micromarkEvents(content), [
+    ["enter", "content", content],
+    ["enter", "paragraph", content],
+    ["enter", "data", "["],
+    ["exit", "data", "["],
+    ["enter", "data", "["],
+    ["exit", "data", "["],
+    ["enter", "data", "|Test]]"],
+    ["exit", "data", "|Test]]"],
+    ["exit", "paragraph", content],
+    ["exit", "content", content],
+  ] as SimplifiedEvent[]);
+});
+
+Deno.test("handle invalid wikilink with blank after pipe", () => {
+  const content = "[[Test|]]";
+  assertEquals(micromarkEvents(content), [
+    ["enter", "content", content],
+    ["enter", "paragraph", content],
+
+    ["enter", "data", "["],
+    ["exit", "data", "["],
+    ["enter", "data", "["],
+    ["exit", "data", "["],
+    ["enter", "data", "Test|]]"],
+    ["exit", "data", "Test|]]"],
+    ["exit", "paragraph", content],
+    ["exit", "content", content],
+  ] as SimplifiedEvent[]);
+});
