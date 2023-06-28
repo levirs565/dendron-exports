25012090dc1e727b09832944f1f9c3afb5bcde6e
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2023-06-28 11:40:17 +0700

    docs: add Renderer docs

--- mod.ts
+++ mod.ts
@@ -9,11 +9,39 @@ export * from "./renderer/lume-njk.ts";
 export type NotePathBuilder = (note: Note) => string;
 
 export interface Options {
+  /**
+   * Directory that all operation is belong to.
+   */
   baseDir: string;
+
+  /**
+   * Vault path to export
+   * Relative to baseDir
+   */
   vaultPath: string;
+
+  /**
+   * Vault name to export
+   * This also used to generate note id when id is not specified.
+   */
   vaultName: string;
 
+  /**
+   * Note and assets export desination
+   * Relative to baseDir
+   */
   noteDest: string;
+
+  /**
+   * Renderer to render single note.
+   */
   noteRenderer: Renderer;
+
+  /**
+   * Function that map note into destination path.
+   * The path will used to resolve link and export path.
+   * The path must not start with / and end with /
+   * Note export path will relative to noteDest
+   */
   notePathBuilder: NotePathBuilder;
 }
